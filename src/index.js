b51d531a30d3ee3c9498b138e208abad8a70db52
S460 <aleksandar.ta6ev@gmail.com>
2020-12-23 20:44:31 +0200

    Todo from can be closed and showed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { projectForm } from "./projectsForm";
 import { createProjects } from "./createProjects";
+import { todoForm } from "./todoForm";
 
 const appFlow = (function () {
 	const startApp = function () {
 		projectForm.startForm();
 		createProjects.startLogic();
+		todoForm.startForm();
 	};
 
 	return { startApp };
--- src/selectProject.js
+++ src/selectProject.js
@@ -1,10 +1,12 @@
 const selectProjectLogic = (function () {
 	const todoSide = document.getElementById("todoSide");
 	const projectName = document.getElementById("currentProject");
+	const addTodoBtn = document.getElementById("addTodoBtn");
 
 	const selectProject = function (project, card) {
 		card.addEventListener("click", () => {
 			projectName.textContent = project.title;
+			addTodoBtn.style.cssText = "display: inline";
 		});
 	};
 
