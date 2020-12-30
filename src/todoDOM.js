4d8a87471c661a9299eb96e32eb977dd92be6c64
S460 <aleksandar.ta6ev@gmail.com>
2020-12-30 08:41:15 +0200

    Edit view is displayed when edit btn is clicked for every todo

--- src/todoDOM.js
+++ src/todoDOM.js
@@ -3,8 +3,8 @@ import { todoLogic } from "./createTodos";
 const todoDOM = (function () {
 	const todoSection = document.getElementById("todoList");
 
-	const clearDiv = function (div) {
-		while (div.childNodes.length !== 0) {
+	const clearDiv = function (div, elementNum) {
+		while (div.childNodes.length !== elementNum) {
 			div.removeChild(div.lastChild);
 		}
 	};
@@ -38,9 +38,9 @@ const todoDOM = (function () {
 		priority.setAttribute("id", "todoPriorityP");
 		priority.textContent = todo.priority;
 
-		let showInfoBtn = document.createElement("p");
-		showInfoBtn.setAttribute("id", "todoInfo");
-		showInfoBtn.textContent = "i";
+		let editBtn = document.createElement("p");
+		editBtn.setAttribute("id", "editTodoBtn");
+		editBtn.textContent = "edit";
 
 		let delBtn = document.createElement("p");
 		delBtn.setAttribute("id", "todoDelBtn");
@@ -56,15 +56,55 @@ const todoDOM = (function () {
 		card.appendChild(title);
 		card.appendChild(dueDate);
 		card.appendChild(priority);
-		card.appendChild(showInfoBtn);
+		card.appendChild(editBtn);
 		card.appendChild(delBtn);
 
 		deleteTodoEvent(delBtn, todo, project, card);
 		changeStateEvent(todo, doneCheckbox);
+		editTodo(editBtn, card, todo);
+	};
+
+	const editTodo = function (btn, card, todo) {
+		btn.addEventListener("click", () => {
+			clearDiv(card, 1);
+			let titleEdit = document.createElement("input");
+			titleEdit.setAttribute("id", "todoTitleEdit");
+			titleEdit.value = todo.title;
+
+			let dueDateEdit = document.createElement("input");
+			dueDateEdit.setAttribute("id", "todoDueDateEdit");
+			dueDateEdit.setAttribute("type", "date");
+			dueDateEdit;
+
+			let priorityEdit = document.createElement("select");
+			priorityEdit.setAttribute("id", "todoPriorityEdit");
+			let priorityOptions = ["High", "Medium", "Low"];
+
+			let saveEdit = document.createElement("p");
+			saveEdit.setAttribute("id", "saveEditedTodo");
+			saveEdit.textContent = "save";
+
+			let cancel = document.createElement("p");
+			cancel.setAttribute("id", "todoDelBtn");
+			cancel.textContent = "cancel";
+
+			card.appendChild(titleEdit);
+			card.appendChild(dueDateEdit);
+			card.appendChild(priorityEdit);
+			priorityOptions.forEach((opt) => {
+				console.log(opt);
+				let option = document.createElement("option");
+				option.textContent = `${opt} Priority`;
+				option.value = opt;
+				priorityEdit.append(option);
+			});
+			card.appendChild(saveEdit);
+			card.appendChild(cancel);
+		});
 	};
 
 	const renderTodos = function (project) {
-		clearDiv(todoSection);
+		clearDiv(todoSection, 0);
 		project.todoList.forEach((todo) => {
 			displayTodo(todo, project);
 		});
